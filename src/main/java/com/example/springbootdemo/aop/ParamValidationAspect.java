8aa0320634e09ee4c3d5c9fffc6ed2d34f000e8a
zhengzhongpeng <zhengzhongpeng@zbj.com>
2019-04-26 18:34:03 +0800

    param validated aspect

--- src/main/java/com/example/springbootdemo/aop/ParamValidationAspect.java
+++ src/main/java/com/example/springbootdemo/aop/ParamValidationAspect.java
@@ -4,10 +4,12 @@ import com.alibaba.fastjson.JSON;
 import com.example.springbootdemo.common_core.Request;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
@@ -26,6 +28,8 @@ import java.util.Set;
  * @author zhengzhongpeng
  * @date 2019/4/26 10:16
  */
+@Aspect
+@Component
 public class ParamValidationAspect {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ParamValidationAspect.class);
